dcac68a70e6d0450d3d162d0877ec90b158eb01a
Javier Andres Charry <Jasics0@users.noreply.github.com>
2020-11-01 00:04:03 -0500

    base

--- BCATelecomunicaciones/src/java/Logic/Criptografia.java
+++ BCATelecomunicaciones/src/java/Logic/Criptografia.java
@@ -11,7 +11,6 @@ import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import org.apache.commons.codec.binary.Base64;
-import org.apache.commons.codec.digest.DigestUtils;
 
 /**
  *
